773fb336646dcb3856b2d0f241d205aa599c601e
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-03-25 10:19:50 -0400

    eeeeEEEEEeeeeh

---
+++ app.js
@@ -0,0 +1,81 @@
+/* let string = 'I am a string';
+let sentence = 'Hello My Dear Friend ' + "My Pinapple";
+let combine = string + sentence; 
+
+
+let student = "Sandra";
+let student2 = "MICHAEL";
+let courseName = "COMPSCI";
+let teacherName = "Whales";
+let sentence = 
+    student +
+    " and " +
+    student2 +
+    " are taking " +
+    courseName +
+    " with " +
+    teacherName;
+
+    let betterSentence = `${student} and 
+    ${student2} are taking ${courseName} with ${teacherName} next year`;
+
+`url/${movie}`*/
+
+//^ these are strings
+
+////////////NUMBERS AND INTEGERS
+
+/* let year = 2020;
+let next year = year + 1;
+let past year = year -1;
+let future = year * 2; */
+
+//////////////////BOOLEANS
+
+/* let trueStatement = true;
+
+/////////Null and Undefined
+
+let richard;
+console.log(richard);
+console.log(jiayu); */
+
+/////////////////BOOLEAN LOGIC OR IF/ELSE
+
+/* let age = 17;
+let baoSquared = "Bao Bao";
+console.log(baoSquared === "Bao Bao");
+console.log(age == "17"); */
+
+/* let age = 45;
+if(age < 18) {
+    console.log("you are a child");
+}   else if (age == 18) {
+    console.log("Happy Bday");
+}   else {
+    console.log("you are over 18");
+} */
+
+///red, green, yellow
+/* let light = "green";
+if (light === "green") {
+    console.log("go!");
+}   else if (light === "red") {
+    console.log("stop!");
+}   else {
+    console.log("slow!");
+} */
+
+
+/* let kristyInt = false;
+let baoInt = false;
+
+if (kristyInt === true && baoInt === true) {
+    console.log("almond 2");
+}   else if (kristyInt === true || baoInt === true) {
+    console.log("almond 1 and milk 1");
+}   else {
+    console.log("milk 2");
+}
+ */
+
